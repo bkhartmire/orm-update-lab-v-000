d65483c495a1ecb8908f2a3aaa4e7ab51cd65b8a
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-09 14:41:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,12 @@ class Student
     student
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    studnet.grade = row[2]
+    student
   end
 
   def self.find_by_name(name)
